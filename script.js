f4b1e8a1148e12434533c01726b9f61e234a5df1
Ashish <chakravartiashish2406@gmail.com>
2023-06-11 15:07:54 +0530

    Saving work

--- script.js
+++ script.js
@@ -103,4 +103,85 @@ tl
 
 
 
+
+
+
+
+
+        // -------------------------------------Page 6----------------------------------------
+
+        let projectDiv2 = document.querySelector(".projectDiv2")
+        let projectDiv3 = document.querySelector(".projectDiv3")
+        let projectDiv4 = document.querySelector(".projectDiv4")
+        let projectDiv5 = document.querySelector(".projectDiv5")
+        let projectDiv6 = document.querySelector(".projectDiv6")
+
+
+        let p6Text = document.querySelector(".p6Text")
+        let p6Text2 = document.querySelector(".p6Text2")
+        let p6Text3 = document.querySelector(".p6Text3")
+        let p6Text4 = document.querySelector(".p6Text4")
+        let p6Text5 = document.querySelector(".p6Text5")
+        let p6Text6 = document.querySelector(".p6Text6")
+        let p6Text7 = document.querySelector(".p6Text7")
+        let p6Text8 = document.querySelector(".p6Text8")
+        let p6Text9 = document.querySelector(".p6Text9")
+        let p6Text10 = document.querySelector(".p6Text10")
+
+        projectDiv2.addEventListener("mouseover",()=>{
+            p6Text.style.color="white"
+            p6Text2.style.color="white"
+            projectDiv2.style.border="none"
+        })
+        projectDiv2.addEventListener("mouseleave",()=>{
+            p6Text.style.color="black"
+            p6Text2.style.color="black"
+         projectDiv2.style.border="1.5px solid #E4E4E4"
+
+        })
+
+
+        projectDiv3.addEventListener("mouseover",()=>{
+            p6Text3.style.color="white"
+            p6Text4.style.color="white"
+            projectDiv3.style.border="none"
+
+        })
+        projectDiv3.addEventListener("mouseleave",()=>{
+            p6Text3.style.color="black"
+            p6Text4.style.color="black"
+         projectDiv3.style.border="1.5px solid #E4E4E4"
+
+        })
+
+        projectDiv4.addEventListener("mouseover",()=>{
+            p6Text5.style.color="white"
+            p6Text6.style.color="white"
+        })
+        projectDiv4.addEventListener("mouseleave",()=>{
+            p6Text5.style.color="black"
+            p6Text6.style.color="black"
+        })
+
+        projectDiv5.addEventListener("mouseover",()=>{
+            p6Text7.style.color="white"
+            p6Text8.style.color="white"
+        })
+        projectDiv5.addEventListener("mouseleave",()=>{
+            p6Text7.style.color="black"
+            p6Text8.style.color="black"
+        })
+
+        projectDiv6.addEventListener("mouseover",()=>{
+            p6Text9.style.color="white"
+            p6Text10.style.color="white"
+        })
+        projectDiv6.addEventListener("mouseleave",()=>{
+            p6Text9.style.color="black"
+            p6Text10.style.color="black"
+        })
+      
+
+
+
     
